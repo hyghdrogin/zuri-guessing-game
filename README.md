07cc6153ac6420027acae55ad403dbc38d33c987
Omopariola Emmanuel <93857121+hyghdrogin@users.noreply.github.com>
2022-10-14 03:55:44 +0000

    Read Me

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # guessing-game
+
+Create a number guessing game to generate a number between the range of 1 and 2
+(https://replit.com/@hyghdrogin/GUESSING-GAME)
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let userName = prompt("Welcome to my Guessing Game, what can I call you?");
 let pointCount = 0;
 
 const guessCheck = (range) => {
-  let level = range - 2;
+  let level = range - 1;
+  let pointCount = level + 1;
   let guess = parseInt(
     prompt(`Hi ${userName}. 
 We have selected a random number between 1 and ${range}.
@@ -16,16 +17,42 @@ We'll tell you if your guess was too high or too low.`)
     console.log(`Your point count is: ${pointCount}`);
     level++;
     console.log(`Welcome to Level ${level}`);
-    rangeLimit++;
+    range++;
   } else if (guess > randomNumber) {
     console.log("Your guess is too high");
     console.log(`You remain in Level ${level}`);
+    if (guess !== randomNumber) {
+      let guess = parseInt(prompt("Try Again"));
+      if (guess == randomNumber) {
+        console.log("You guessed the right number");
+        pointCount++;
+        console.log(`Your point count is: ${pointCount}`);
+        level++;
+        console.log(`Welcome to Level ${level}`);
+        range++;
+      } else {
+        console.log("Keep Trying");
+      };
+    };
   } else if (guess < randomNumber) {
     console.log("Your guess is too low");
     console.log(`You remain in Level ${level}`);
+    if (guess !== randomNumber) {
+      let guess = parseInt(prompt("Try Again"));
+      if (guess == randomNumber) {
+        console.log("You guessed the right number");
+        pointCount++;
+        console.log(`Your point count is: ${pointCount}`);
+        level++;
+        console.log(`Welcome to Level ${level}`);
+        range++;
+      } else {
+        console.log("Keep Trying");
+      };
+    };
   } else {
     console.log("Invalid Guess");
     console.log(`You remain in Level ${level}`);
-  }
+  };
 };
-guessCheck();
+guessCheck(8);
