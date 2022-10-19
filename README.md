ca6e9ceec7802cfdbb88440d7405b9924b576cc3
Omopariola Emmanuel <93857121+hyghdrogin@users.noreply.github.com>
2022-10-19 02:39:04 +0000

    final edit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # guessing-game
 
-Create a number guessing game to generate a number between the range of 1 and 2
 (https://replit.com/@hyghdrogin/GUESSING-GAME)
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 let userName = prompt("Welcome to my Guessing Game, what can I call you?");
-let pointCount = 0;
 
 const guessCheck = (range) => {
-  let level = range - 1;
-  let pointCount = level + 1;
   let guess = parseInt(
     prompt(`Hi ${userName}. 
 We have selected a random number between 1 and ${range}.
 See if you can guess it in 2 turns or fewer.
 We'll tell you if your guess was too high or too low.`)
   );
+  let level = range - 1;
+  let pointCount = level - 1;
   let randomNumber = Math.floor(Math.random() * range) + 1;
   if (guess == randomNumber) {
     console.log("You guessed the right number");
+    level++;
     pointCount++;
     console.log(`Your point count is: ${pointCount}`);
-    level++;
     console.log(`Welcome to Level ${level}`);
-    range++;
   } else if (guess > randomNumber) {
     console.log("Your guess is too high");
     console.log(`You remain in Level ${level}`);
@@ -31,7 +29,7 @@ We'll tell you if your guess was too high or too low.`)
         console.log(`Welcome to Level ${level}`);
         range++;
       } else {
-        console.log("Keep Trying");
+        console.log("Restart Level");
       };
     };
   } else if (guess < randomNumber) {
@@ -47,7 +45,7 @@ We'll tell you if your guess was too high or too low.`)
         console.log(`Welcome to Level ${level}`);
         range++;
       } else {
-        console.log("Keep Trying");
+        console.log("Restart Level");
       };
     };
   } else {
@@ -55,4 +53,4 @@ We'll tell you if your guess was too high or too low.`)
     console.log(`You remain in Level ${level}`);
   };
 };
-guessCheck(8);
+guessCheck(2);
